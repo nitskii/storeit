22b65d8edbedde8c8e1eae489772784c1c8598be
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-08-06 13:19:57 +0300

    add home page markdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,54 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [ "./src/views/*.html" ],
-    plugins: [ import("@tailwindcss/forms") ]
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/views/*.html"],
+  plugins: [import("@tailwindcss/forms")],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          300: "#ffbf00",
+          600: "#ffa500",
+          700: "#e69500",
+          800: "#ff8c00",
+        },
+      },
+    },
+    fontFamily: {
+      body: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      sans: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
+  },
 };
