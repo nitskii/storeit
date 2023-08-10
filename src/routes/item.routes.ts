1c198143ed58999823adaff87f312a33105017d5
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-08-10 20:27:00 +0300

    add tags to item card

--- src/routes/item.routes.ts
+++ src/routes/item.routes.ts
@@ -29,29 +29,38 @@ const itemRoutes = (app: Elysia) => app
         async ({ html }) => {
             const items = await itemService.getAll();
 
-            // return html(items.map(item => (`
-            //   <div class="group relative">
-            //     <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-            //       <img
-            //         src="${item.image}"
-            //         alt="${item.id}"
-            //         class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
-            //     </div>
-            //     <div class="mt-4 flex justify-between">
-            //       <div>
-            //         <h3 class="text-sm text-gray-700">
-            //           <a href="#">
-            //             <span aria-hidden="true" class="absolute inset-0"></span>
-            //             ${item.name}
-            //           </a>
-            //         </h3>
-            //         ${ item.location && `<p class="mt-1 text-sm text-gray-500">${item.location}</p>` }
-            //       </div>
-            //     </div>
-            //   </div>
-            // `)).join(""));
-
-            return items;
+            return html(items.map(item => (`
+              <div class="group relative">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                  <img
+                    src="${item.image}"
+                    alt="${item.id}"
+                    class="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+                </div>
+                <div class="mt-4 flex justify-between">
+                  <div>
+                    <h3 class="text-sm text-gray-700">
+                      <a href="#">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${item.name}
+                      </a>
+                    </h3>
+                    ${ item.location && `<p class="mt-1 text-sm text-gray-500">${item.location}</p>` }
+                    <div class="mt-1">
+                        ${
+                            item.tags
+                                .map(t => `
+                                    <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded text-gray-600 bg-gray-300 last:mr-0 mr-1">
+                                        ${t}
+                                    </span>
+                                `)
+                                .join("")
+                        }
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `)).join(""));
         }
     );
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/**/*.{html,tsx}"
+    "./src/**/*.{html,ts}"
   ],
   plugins: [
     require("@tailwindcss/forms"),
