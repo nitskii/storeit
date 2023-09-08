e2e5ab49a7fc0c4b5634a349d26e8192d414dbf6
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-09-08 13:15:57 +0300

    add status display to logger middleware

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -7,21 +7,43 @@ const COLOR_BLUE = '\x1B[38;5;33m';
 const COLOR_YELLOW = '\x1B[38;5;11m';
 const COLOR_ORANGE = '\x1B[38;5;202m';
 const COLOR_RED = '\x1B[38;5;9m';
+const COLOR_CYAN='\x1B[38;5;51m';
 
 const methodToColor = new Map();
-methodToColor.set('POST', COLOR_GREEN);
-methodToColor.set('GET', COLOR_BLUE);
-methodToColor.set('PUT', COLOR_YELLOW);
-methodToColor.set('PATCH', COLOR_ORANGE);
-methodToColor.set('DELETE', COLOR_RED);
+
+methodToColor.set(  'POST',  COLOR_GREEN);
+methodToColor.set(   'GET',   COLOR_BLUE);
+methodToColor.set(   'PUT', COLOR_YELLOW);
+methodToColor.set( 'PATCH', COLOR_ORANGE);
+methodToColor.set('DELETE',    COLOR_RED);
+
+const statusToColor = new Map();
+
+statusToColor.set(200,  COLOR_GREEN);
+statusToColor.set(201,  COLOR_GREEN);
+statusToColor.set(204,  COLOR_GREEN);
+statusToColor.set(302,   COLOR_CYAN);
+statusToColor.set(304,   COLOR_CYAN);
+statusToColor.set(400, COLOR_ORANGE);
+statusToColor.set(401, COLOR_ORANGE);
+statusToColor.set(404, COLOR_ORANGE);
+statusToColor.set(409, COLOR_ORANGE);
+statusToColor.set(500,    COLOR_RED);
+
+const MAX_METHOD_LENGTH = 6;
+const MAX_PATH_LENGTH = 25;
 
 const logger = (app: Elysia) => app
-  .onRequest(({ request }) => {
-    const { method, url } = request;
-    const { pathname: path } = new URL(url);
-    const color = methodToColor.get(method);
+  .onResponse(({ request: { method }, path, set: { status } }) => {
+    const methodColor = methodToColor.get(method);
+    const statusColor = statusToColor.get(status);
 
-    console.log(`${F_BOLD}${color}${method}${NO_FORMAT} ${path}`);
+    const result = 
+      `${F_BOLD}${methodColor}${method}${NO_FORMAT}${' '.repeat(MAX_METHOD_LENGTH - method.length)}` +
+      ` ${path} ${' '.repeat(MAX_PATH_LENGTH - path.length)} - ` + 
+      `${F_BOLD}${statusColor}${status}${NO_FORMAT}`;
+
+    console.log(result);
   });
 
 export default logger;
