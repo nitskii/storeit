3c27e123966da7c8ccaa3a0b008a3f50643d98d2
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-09-30 22:34:33 +0300

    adapt item modal to ZPA

--- public/main.js
+++ public/main.js
@@ -61,3 +61,88 @@ const handleLoginResult = (detail) => {
       );
   }
 };
+
+htmx.replaceClass = (elt, oldClass, newClass) => {
+  htmx.addClass(elt, newClass);
+  htmx.removeClass(elt, oldClass);
+}
+
+const showItemModal = () => {
+  htmx.replaceClass(htmx.find('#item-modal'), "hidden", "flex");
+}
+
+const hideItemModal = () => {
+  htmx.replaceClass(htmx.find('#item-modal'), "flex", "hidden");
+}
+
+const tagExistsMessage = document.createElement('div');
+tagExistsMessage.innerText = 'Тег вже додано';
+htmx.addClass(tagExistsMessage, 'pl-2');
+htmx.addClass(tagExistsMessage, 'pt-1');
+htmx.addClass(tagExistsMessage, 'text-red-500');
+
+const addTagToList = () => {
+  const tagInput = htmx.find('#tag-input');
+
+  if (tagInput.value == "") {
+    return;
+  }
+
+  const addedTagsList = htmx.find('#added-tags-list');
+  const addedTags = addedTagsList.children;
+
+  for (let addedTag of addedTags) {
+    if (addedTag.innerText == tagInput.value) {
+      htmx
+        .find('#tag-input-block')
+        .append(tagExistsMessage);
+      tagInput
+        .addEventListener(
+          'input',
+          () => (htmx.remove(tagExistsMessage)),
+          { once: true }
+        );
+
+      return;
+    }
+  }
+
+  const newTag = document.createElement('li');
+
+  const crossImage = document.createElement('img');
+  crossImage.src = '/public/cross.svg';
+  htmx.addClass(crossImage, 'w-6');
+  htmx.addClass(crossImage, 'h-6');
+
+  const removeButton = document.createElement('button');
+  removeButton.type = 'button';
+  htmx.addClass(removeButton, 'p-1');
+  removeButton.append(crossImage);
+  removeButton.addEventListener(
+    'click',
+    () => {
+      htmx.remove(newTag);
+      addedTagsList.childElementCount || htmx.replaceClass(addedTagsList, 'flex', 'hidden');
+    }, {
+      once: true
+    }
+  );
+
+  const tagHiddenInput = document.createElement('input');
+  tagHiddenInput.type = 'hidden';
+  tagHiddenInput.value = tagInput.value;
+  tagHiddenInput.name = 'tags';
+
+  htmx.addClass(newTag, 'flex');
+  htmx.addClass(newTag, 'items-center');
+  htmx.addClass(newTag, 'rounded-lg');
+  htmx.addClass(newTag, 'bg-orange-200');
+  htmx.addClass(newTag, 'pl-2');
+  newTag.append(tagHiddenInput);
+  newTag.append(tagInput.value);
+  newTag.append(removeButton);
+
+  addedTagsList.append(newTag);
+  htmx.replaceClass(addedTagsList, 'hidden', 'flex');
+  tagInput.value = '';
+}
--- src/components/IndexPageContent.tsx
+++ src/components/IndexPageContent.tsx
@@ -6,6 +6,12 @@ const IndexPageContent = () => (
     <header class="bg-orange-100">
       <nav class="flex justify-end space-x-2 p-2">
         <button
+          id="button-show-item-modal"
+          class="rounded-lg bg-orange-200 p-2 hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-300"
+          onclick="showItemModal()">
+          Додати
+        </button>
+        <button
           class="rounded-lg bg-orange-200 p-2 hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-300"
           hx-post="/api/logout">
           Вийти
@@ -17,7 +23,9 @@ const IndexPageContent = () => (
       hx-get="/api/items"
       hx-trigger="load, itemsUpdate from:body"
     />
-    <div class="fixed left-0 top-0 hidden h-screen w-full items-center justify-center bg-black bg-opacity-50 px-4">
+    <div
+      id="item-modal"
+      class="fixed left-0 top-0 hidden h-screen w-full items-center justify-center bg-black bg-opacity-50 px-4">
       <div class="flex w-full flex-col items-center justify-center space-y-4 rounded-lg bg-orange-100 p-4 shadow sm:max-w-md">
         <form class="flex w-full flex-col items-center space-y-4">
           <div class="w-full">
@@ -41,28 +49,27 @@ const IndexPageContent = () => (
             class="w-full rounded-lg bg-orange-200 p-2 hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-300">
             Оберіть локацію
           </button>
-          <div class="flex w-full flex-col justify-between">
-            <ul 
-              id="selected-tags-list"
-              class="mb-4 hidden flex-wrap gap-2"
-            />
+          <div id="tag-input-block" class="flex w-full flex-col justify-between">
+            <ul id="added-tags-list" class="mb-4 hidden flex-wrap gap-2" />
             <div class="flex w-full overflow-hidden rounded-lg">
-              <input
-                id="tag-input"
-                list="tags-datalist"
-                type="text"
-                class="block w-full border-0 bg-orange-200 placeholder:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-0 focus-visible:outline-orange-300"
-                placeholder="Теги"
-              />
+              <div class="w-full">
+                <input
+                  id="tag-input"
+                  list="tags-datalist"
+                  type="text"
+                  class="block w-full border-0 bg-orange-200 placeholder:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-0 focus-visible:outline-orange-300"
+                  placeholder="Теги"
+                />
+              </div>
               <datalist
                 id="tags-datalist"
                 hx-get="/api/tags"
-                hx-trigger="load"
+                hx-trigger="click from:button-show-item-modal"
               />
               <button
-                id="button-add-tag"
                 type="button"
-                class="border-l-2 border-orange-100 bg-orange-200 px-4 py-2">
+                class="border-l-2 border-orange-100 bg-orange-200 px-4 py-2"
+                onclick="addTagToList()">
                 <img src="/public/plus.svg" class="h-6 w-6" />
               </button>
             </div>
@@ -71,8 +78,6 @@ const IndexPageContent = () => (
             </div> */}
           </div>
           <button
-            id="button-submit-new-item"
-            type="submit"
             class="w-full rounded-lg bg-orange-200 p-2 hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-300"
             hx-post="/api/item"
             hx-encoding="multipart/form-data">
@@ -80,13 +85,8 @@ const IndexPageContent = () => (
           </button>
         </form>
         <button
-          type="button"
-          class="w-full rounded-lg bg-orange-200 p-2 hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-300">
-          Назад
-        </button>
-        <button
-          type="button"
-          class="w-full rounded-lg bg-orange-200 p-2 hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-300">
+          class="w-full rounded-lg bg-orange-200 p-2 hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-300"
+          onclick="hideItemModal()">
           Закрити
         </button>
       </div>
