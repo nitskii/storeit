ef9721e446e2cabeac652877823b2067afcaed7d
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-10-11 14:43:58 +0300

    created custom logger for db queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,7 @@
 import { createClient } from '@libsql/client';
+import { Logger } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/libsql';
+import { format } from 'sql-formatter';
 import * as schema from './schema';
 
 const client = createClient({
@@ -7,6 +9,23 @@ const client = createClient({
   authToken: process.env.DATABASE_AUTH_TOKEN
 });
 
-const db = drizzle(client, { schema, logger: true }); 
+class PrettyLogger implements Logger {
+  logQuery(query: string, params: unknown[]): void {
+    console.log(
+      format(query, {
+        language: 'sqlite',
+        keywordCase: 'upper',
+        tabWidth: 2,
+        indentStyle: 'tabularRight',
+        params: params.map((p) => String(typeof p == 'string' ? `"${p}"` : p))
+      })
+    );
+  }
+}
+
+const db = drizzle(client, {
+  schema,
+  logger: process.env.NODE_ENV == 'development' && new PrettyLogger()
+});
 
 export default db;
