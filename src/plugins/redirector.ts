8bd5550bef5d0bf1ab7da5cdf61e1a5e96d635ca
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-10-27 16:07:36 +0300

    fixed redirector plugin allowing non-authorized access

--- src/plugins/redirector.ts
+++ src/plugins/redirector.ts
@@ -3,6 +3,8 @@ import jwt from '@elysiajs/jwt';
 import Elysia from 'elysia';
 import userService from '../services/user-service';
 
+const itemPathRegex = /^\/item\/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+
 const redirector = new Elysia({ name: 'redirector' })
   .use(cookie())
   .use(
@@ -16,15 +18,21 @@ const redirector = new Elysia({ name: 'redirector' })
       
       if (payload && await userService.existsById(payload.sub!)) {
         set.redirect = '/';
+
+        return "";
       }
-    } else if (path == '/' && cookie.auth) {
+    } else if ((['/', '/locations'].includes(path) || itemPathRegex.test(path)) && cookie.auth) {
       const payload = await jwt.verify(cookie.auth);
       
       if (!payload || !await userService.existsById(payload.sub!)) {
         set.redirect = '/login';
+
+        return "";
       }
-    } else if (path == '/' && !cookie.auth){
+    } else if ((['/', '/locations'].includes(path) || itemPathRegex.test(path)) && !cookie.auth) {
       set.redirect = '/login';
+
+      return "";
     }
   });
 
