9afff6fe0e170e415d98776f57a2d49f306ae04b
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-10-31 16:23:09 +0200

    fix auth cookie verification

--- src/plugins/authenticator.ts
+++ src/plugins/authenticator.ts
@@ -14,17 +14,13 @@ const authenticator = new Elysia({ name: 'authenticator' })
         throw new HttpError("Не авторізовано", "UNAUTHORIZED", 401);
       }
 
-      const payload = await jwt.verify(auth.value);
-
-      console.log(payload);
+      const payload = await jwt.verify(`${auth}`);
 
       if (!payload) {
         throw new HttpError("Термін дії токену закінчився", "JWT_EXPIRED", 401);
       }
 
-      return {
-        userId: payload.sub!
-      };
+      return { userId: payload.sub };
     }
   );
     
