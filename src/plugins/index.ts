25217ccf79b0565462aa94b151e6d050c428ccef
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-10-31 23:30:02 +0200

    removed error handler plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,11 +1,9 @@
 import authenticator from './authenticator';
-import errorHandler from './error-handler';
 import logger from './logger';
 import redirector from './redirector';
 
 export {
   authenticator,
-  errorHandler,
   logger,
   redirector
 };
